f5f02ba65b20bc5ad712e23cb284b7ccaeef5c44
NagajothiS <32875392+NagajothiS@users.noreply.github.com>
2025-12-22 14:56:35 +0530

    Committing Day02 activity

---
+++ Day02/Primitive-Datatypes.js
@@ -0,0 +1,11 @@
+let firstName = "Nagajothi"
+let companyName = null
+let mobileNumber = 9790667125
+let isAutomation = true
+let hasPlaywright 
+console.log("firstName : " + firstName + " " + typeof(firstName))
+console.log("companyname : " + companyName +  " " + typeof(companyName))
+console.log("mobileNumber : " + mobileNumber +  " " +  typeof(mobileNumber))
+console.log("isAutomation : " + isAutomation +  " " + typeof(isAutomation))
+console.log("hasPlaywright : " + hasPlaywright +  " " +  typeof(hasPlaywright))
+
